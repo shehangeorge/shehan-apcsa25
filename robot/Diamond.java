83798b440299dc429f2c9e989a802f8780d098c9
shehangeorge <sg7310@pleasantonusd.net>
2025-08-26 20:54:22 +0000

    math.java

--- robot/Diamond.java
+++ robot/Diamond.java
@@ -9,21 +9,71 @@ public class Diamond implements Directions{
         World.setVisible(true);// allows us to see the run output
         // the bigger the street, the farther north
         World.setSize(20,20);
-
+        World.setDelay(3);
 
         // The line below creates a Robot that we will refer to as rob
         // Find out what the numbers and direction do!
         // Put rob in a better location for your initials.
-        Robot rob = new Robot(15,2,South,9);
+       // Robot rob = new Robot(15,2,South,9);
 
+         Robot rob = new Robot(15,2,South,900);
+
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.move();
+        rob.move();
+        rob.move();
+        rob.move();
+        rob.move();
+        
+      
+        for (int i =1; i<=4; i++) {
+            rob.putBeeper();
+            rob.turnLeft();
+            rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        }
         // Want a second robot?  No prob.  They are cheap :)
         //Robot dude = new Robot(7,5,West,9);
         // examples of commands you can invoke on a Robot
-        rob.move();// move one step in the direction it is facing
+      //  rob.move();// move one step in the direction it is facing
 
 
         // starting the letter R
-        rob.putBeeper();
+        //rob.putBeeper();
 
 
 
--- robot/Starter.java
+++ robot/Starter.java
@@ -9,33 +9,176 @@ public class Starter implements Directions{
     World.setVisible(true);// allows us to see the run output
     // the bigger the street, the farther north
     World.setSize(20,20);
+     World.setDelay(3);
 
 
     // The line below creates a Robot that we will refer to as rob
     // Find out what the numbers and direction do!
     // Put rob in a better location for your initials.
-    Robot rob = new Robot(15,2,South,9);
+    Robot rob = new Robot(16,3,South,900);
+    
 
     // Want a second robot?  No prob.  They are cheap :)
     //Robot dude = new Robot(7,5,West,9);
     // examples of commands you can invoke on a Robot
     rob.move();// move one step in the direction it is facing
+    rob.turnLeft();
 
 
     // starting the letter R
-    rob.putBeeper();
-    rob.putBeeper();
-    rob.putBeeper();
+ rob.move();
+    rob.move();
+    rob.move();
+    rob.move();
+    rob.move();
+    rob.turnLeft();
+    rob.turnLeft();
     rob.putBeeper();
     rob.move();
     rob.putBeeper();
     rob.move();
-    rob.move();
+    rob.putBeeper();
     rob.move();
     rob.putBeeper();
     rob.move();
+    rob.putBeeper();
+    rob.move();
+  
+    rob.turnLeft();
     rob.move();
     rob.putBeeper();
+    rob.move();
+    rob.putBeeper();
+    rob.move();
+    rob.putBeeper();
+    rob.move();
+   
+    rob.turnLeft();
+    rob.move();
+    rob.putBeeper();
+    rob.move();
+    rob.putBeeper();
+    rob.move();
+    rob.putBeeper();
+    rob.move();
+    rob.putBeeper();
+    rob.move();
+    rob.turnLeft();
+    rob.turnLeft();
+    rob.turnLeft();
+    rob.move();
+    rob.putBeeper();
+    rob.move();
+    rob.putBeeper();
+    rob.move();
+    rob.putBeeper();
+    rob.move();
+    rob.turnLeft();
+    rob.turnLeft();
+    rob.turnLeft();
+    rob.move();
+    rob.putBeeper();
+    rob.move();
+    rob.putBeeper();
+    rob.move();
+    rob.putBeeper();
+    rob.move();
+    rob.putBeeper();
+    rob.move();
+    rob.putBeeper();
+    rob.move();
+    rob.turnLeft();
+    rob.move();
+    rob.move();
+    rob.move();
+    rob.move();
+    rob.move();
+    rob.move();
+    rob.turnLeft();
+    rob.turnLeft();
+    rob.turnLeft();
+    rob.move();
+    rob.move();
+    rob.move();
+    
+    
+    Robot dude = new Robot(15,15,West,900);
+    dude.putBeeper();
+    dude.move();
+    dude.putBeeper();
+    dude.move();
+    dude.putBeeper();
+    dude.move();
+    dude.putBeeper();
+    dude.move();
+    dude.putBeeper();
+    dude.move();
+    dude.turnLeft();
+    dude.move();
+    dude.putBeeper();
+    dude.move();
+    dude.putBeeper();
+    dude.move();
+    dude.putBeeper();
+    dude.move();
+    dude.putBeeper();
+    dude.move();
+    dude.putBeeper();
+    dude.move();
+    dude.putBeeper();
+    dude.move();
+    dude.putBeeper();
+    dude.move();
+    dude.turnLeft();
+    dude.move();
+    dude.putBeeper();
+    dude.move();
+    dude.putBeeper();
+    dude.move();
+    dude.putBeeper();
+    dude.move();
+    dude.putBeeper();
+    dude.move();
+    dude.turnLeft();
+    dude.move();
+    dude.putBeeper();
+    dude.move();
+    dude.putBeeper();
+    dude.move();
+    dude.putBeeper();
+    dude.move();
+    dude.turnLeft();
+    dude.move();
+    dude.putBeeper();
+    dude.move();
+    dude.putBeeper();
+    dude.turnLeft();
+    dude.turnLeft();
+    dude.move();
+    dude.move();
+    dude.putBeeper();
+    dude.move();
+    dude.putBeeper();
+    dude.move();
+    dude.putBeeper();
+    dude.turnLeft();
+    dude.turnLeft();
+    dude.turnLeft();
+    dude.move();
+    dude.putBeeper();
+    dude.move();
+    dude.putBeeper();
+    dude.move();
+    dude.putBeeper();
+    dude.move();
+    dude.putBeeper();
+    dude.move();
+    dude.move();
+    dude.move();
+    dude.move();
+    dude.move();
+    dude.move();
+    dude.move();
 
     // done with the line, now on the curve
     // rob.turnLeft();
