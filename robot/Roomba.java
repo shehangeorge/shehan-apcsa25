131a5fb61bec9ba1728c2e327e108fc99fe66376
shehangeorge <sg7310@pleasantonusd.net>
2025-09-15 22:05:46 +0000

    finalFINALnobugWITHCOMMENTEXPLANATIONS

--- robot/Roomba.java
+++ robot/Roomba.java
@@ -11,28 +11,22 @@ public class Roomba implements Directions {
 		// LEAVE THIS ALONE!!!!!!
 		//String worldName = "robot/basicRoom.wld";
 		String worldName = "robot/finalTestWorld2024 (1).wld";
+//the world has to be changed to the final testing world in order to be seen in the port
 
-		
 		Roomba cleaner = new Roomba();
 		int totalBeepers = cleaner.cleanRoom(worldName, 25, 11);
 		System.out.println("Roomba cleaned up a total of " + totalBeepers + " beepers.");
-
 	}
-
 	// declared here so it is visible in all the methods!
 	private Robot roomba;
-
 	int totalBeepers = 0;
-	
 	// You will need to add many variables!!
 
 
 	public int cleanRoom(String worldName, int startX, int startY) {
-
-		// A new Robot should be constructed and assigned to the global (instance) variable named roomba that is declared above.
-        // Make sure it starts at startX and startY location.
+		// *A new Robot should be constructed and assigned to the global (instance) variable named roomba that is declared above.
+        // *Make sure it starts at startX and startY location.
 		roomba = new Robot(26, 149, West, 0);
-
 		World.readWorld(worldName);
 		World.setVisible(true);
 		World.setDelay(0);
@@ -44,8 +38,8 @@ public class Roomba implements Directions {
 		 * large, complex task into smaller, easier to solve problems.
 		 */
 
-		// the line below causes a null pointer exception
-		// what is that and why are we getting it
+		// *the line below causes a null pointer exception
+		// *what is that and why are we getting it
 
 		int totalArea = 0;
 
@@ -67,39 +61,32 @@ public class Roomba implements Directions {
 			if (roomba.nextToABeeper()){
 				previousPile = 0;
 				numberOfPiles++;
-
+//in order to total up the beepers into one value at the end
 		
 			while(roomba.nextToABeeper()){
-
 				totalBeepers++;
 				roomba.pickBeeper();
 				previousPile++;
-
+//so the beepers can be added to the total number to be displayed at the end
 
 				if (previousPile>largestPile){
-
-					largestPile = previousPile;
-					
+					largestPile = previousPile;	
+//in order for the largest pile to be determined at the end, including how many beepers it has and its location	
 					st = roomba.street();
 					av = roomba.avenue();
+//so the location can be displayed as coordinates
 				}
 			}
-
 			}
-
 			if (!roomba.frontIsClear()){
-
 			if(roomba.facingEast()){
-
 				roomba.turnLeft();
 				roomba.move();
 				totalArea++;
 				roomba.turnLeft();
-				
+//explanation at the bottom -- basically when it's looking east it turns to the left and vice versa
 			}
-
 			else {
-
 				roomba.turnLeft();
 				roomba.turnLeft();
 				roomba.turnLeft();
@@ -108,29 +95,24 @@ public class Roomba implements Directions {
 				roomba.turnLeft();
 				roomba.turnLeft();
 				roomba.turnLeft();
-
-			}
-
-		
-			
+//follows Ms. Cyr's snaking method, where the roomba goes back and forth -- when it hits a wall, the code tells it which direction to u-turn in so it can keep looking for beepers and not stay stuck at a wall
+			}		
 		}
-
-
-		
 	}
 
 	System.out.println("STATISTICS");
 	System.out.println("the total area of the world: ");
 	System.out.println(totalArea);
-	System.out.print("the total number of piles: ");
+	System.out.print("total number of piles: ");
 	System.out.println(numberOfPiles);
-	System.out.println("the percent dirty: ");
+	System.out.println("percent of the world that was dirty: ");
 	System.out.println(100* numberOfPiles/totalArea);
-	System.out.print("the average pile size: ");
+	System.out.print("average pile size: ");
 	System.out.println((totalBeepers/numberOfPiles));
-	System.out.println("Location of Largest Pile: (" + st + ","  + av + ")" );
-	System.out.print("largest pile has: ");
+	System.out.print("the largest pile has: ");
 	System.out.println(largestPile);
+	System.out.println("the location of largest pile: (" + st + ","  + av + ")" );
+	
 	return totalBeepers;
 
 
@@ -138,3 +120,4 @@ public class Roomba implements Directions {
 
 }
 }
+//most comments with a * were written by Ms. Cyr, while all the other explanations of each segment of the code were written by me
