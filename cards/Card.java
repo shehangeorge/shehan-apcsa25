721b99b709a21a474e5f1a36a144b736d8d2ecdf
shehangeorge <sg7310@pleasantonusd.net>
2025-12-03 22:30:18 +0000

    forcedcommit

--- cards/Card.java
+++ cards/Card.java
@@ -7,7 +7,7 @@ public class Card {
     private static final String[] SUITS = {"♦", "♣", "♥", "♠"};
     private static final String[] VALUES = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 
-    public Card(int suit, int value) { //constructor to only use valid range
+    public Card(int suit, int value) { //constructor to only use valid range --> I believe this fixes the out of bounds error
         if (suit < 0) this.suit = 0;
         else if (suit > 3) this.suit = 3;
         else this.suit = suit;
