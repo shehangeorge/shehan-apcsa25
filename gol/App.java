d40c958d40c9055ff816eee7949af12bf9ce9f8b
shehangeorge <sg7310@pleasantonusd.net>
2025-12-15 21:24:53 +0000

    first part complete

--- gol/App.java
+++ gol/App.java
@@ -4,17 +4,17 @@ public class App {
 
     public static void main(String[] args)
     {
-        // GameOfLife g = new GameOfLife(7, 7);
+        GameOfLife g = new GameOfLife(7, 7);
 
-        // int[][] data = {
-        //     { 0, 1, 0},
-        //     { 0, 0, 1},
-        //     { 1, 1, 1 }
-        // };
+         int[][] data = {
+             { 0, 1, 0},
+             { 0, 0, 1},
+             { 1, 1, 1 }
+         };
 
-        // g.set(1, 1, data);
+         g.set(1, 1, data);
 
-        // g.run(5);
+         g.run(5);
 
         TestSuite.run();
     }
--- gol/GameOfLife.java
+++ gol/GameOfLife.java
@@ -1,26 +1,19 @@
 package gol;
 
-import java.util.Arrays;
-
 public class GameOfLife implements Board {
-
-    // Integers: 0 or 1 for alive or dead
-    private int[][] board;
-
-    public GameOfLife(int x, int y)
-    {
-        // Construct a 2d array of the given x and y size.
+    private int[][] grid;
+    
+    public GameOfLife(int rows, int cols) {
+        grid = new int[rows][cols];
     }
-
-    // Set values on the board
-    public void set(int x, int y, int[][] data) {
-        for (int i = 0; i < data.length; i++) {
-            for (int j = 0; j < data[0].length; j++) {
-                board[i + x][j + y] = data[i][j];
+    
+    public void set(int r, int c, int[][] pattern) {
+        for (int i = 0; i < pattern.length; i++) {
+            for (int j = 0; j < pattern[0].length; j++) {
+                grid[(i + r) % grid.length][(j + c) % grid[0].length] = pattern[i][j];
             }
         }
     }
-
     // Run the simulation for a number of turns
     public void run(int turns) {
         // call step the number of times requested
