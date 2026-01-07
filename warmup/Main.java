c583d877d1b05902aa60d0ce1cd9b747e99cf79a
shehangeorge <sg7310@pleasantonusd.net>
2026-01-07 22:35:16 +0000

    warmupprojectcompleted(problem1)

--- warmup/Main.java
+++ warmup/Main.java
@@ -4,6 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         // Your code here.
+        System.out.println(sum(1000));
 
+    }
+    static int sum (int limit) {
+        int total=0;
+        for (int i = 1; i<limit; i++){
+            if (i%3==0 || i%5==0){
+                total+=i;
+            }
+        }
+        return total;
     }
 }
