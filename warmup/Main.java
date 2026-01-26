3e7701e3862d3972fb351984667eda4c5ccf69a2
shehangeorge <sg7310@pleasantonusd.net>
2026-01-26 21:50:25 +0000

    idk I saw a change

--- warmup/Main.java
+++ warmup/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println(sum(1000));
 
     }
-    static int sum (int limit) {
+    int sum (int limit) {
         int total=0;
         for (int i = 1; i<limit; i++){
             if (i%3==0 || i%5==0){
