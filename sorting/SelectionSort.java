9507d9a30ae867e00209d5d938ad21c3ea98860c
shehangeorge <sg7310@pleasantonusd.net>
2026-02-03 22:39:15 +0000

    selection working

---
+++ sorting/SelectionSort.java
@@ -0,0 +1,22 @@
+package sorting;
+
+
+public class SelectionSort implements Sorter {
+
+
+    public void sort(int[] input) {
+        for (int i = 0; i < input.length - 1; i++) {
+            int minIndex = i;
+           
+            for (int j = i + 1; j < input.length; j++) {
+                if (input[j] < input[minIndex]) {
+                    minIndex = j;
+                }
+            }
+           
+            int temp = input[minIndex];
+            input[minIndex] = input[i];
+            input[i] = temp;
+        }
+    }
+}
